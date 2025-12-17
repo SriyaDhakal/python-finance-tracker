bd858d9fff4d99abe9d19f6be53fcbe7647e4f01
Sriya Dhakal <sriyadhakal@Sriyas-MacBook-Air.local>
2025-12-17 17:14:12 -0600

    Day 1: Python program - Hello World

---
+++ phase1_basics/hello_world.py
@@ -0,0 +1,12 @@
+print("Hello, World!")
+print("Welcome to Python Finance Tracker!")
+
+
+ #Print multiple lines
+print("=" * 40)
+print("Let's learn Python together!")
+print("=" * 40)
+
+# Math in print
+print("5 + 3 =", 5 + 3)
+print("10 - 4 =", 10 - 4)
